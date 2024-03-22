422540e1843c1dd302e429641cb0ca1ed09c30c5
Achmad Farchan Hadi <fachmad78@gmail.com>
2024-03-22 20:59:43 +0700

    fix: removed unused variable

--- src/components/right-bar.tsx
+++ src/components/right-bar.tsx
@@ -1,6 +1,6 @@
 const RightBar = () => {
   return (
-    <div className="flex flex-row w-96 max-h-full h-dvh h-screen sticky top-0">
+    <div className="flex flex-row w-96 max-h-full h-screen sticky top-0">
       <img src="/src/assets/pattern.png" alt="bg" className="object-cover" />
     </div>
   );
